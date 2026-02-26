9a0c98960f25be419f4055156405683a26c28f1d
pfcinfo4-sketch <pfcinfo4@gmail.com>
2026-02-27 00:21:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 /* ============================== CONFIGURATION SUPABASE ================================= */
 
 $project_url = "https://xrsebcwlbekugvpwqcii.supabase.co";
-$api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhyc2ViY3dsYmVrdWd2cHdxY2lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNTkxNzksImV4cCI6MjA4NzYzNTE3OX0.laxq2_POjoitWv8J0RQNo5SiBXvs2yzmtAaoxWje7Vo"; // ⚠️ Remplace par ta vraie clé anon publique
+$api_key = "sb_publishable_8yU4oGR-SPOduPtH-Qsi-Q_GH8HMO9y"; // ⚠️ Remplace par ta vraie clé anon publique
 
 /* ============================== LOGIN SUPABASE ================================= */
 
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
     $password_input = trim($_POST["password"]);
 
     // 🔥 On filtre directement par email (plus rapide)
-    $url = $project_url . "/rest/v1/student?email=eq." . urlencode($email);
+       $url = $project_url . "/rest/v1/student?select=*";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -163,4 +163,5 @@ if (isset($_GET["logout"])) {
 
 </body>
 </html>
+
 
